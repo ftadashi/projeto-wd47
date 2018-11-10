fd11af2cb14f750d1d7a4547341f80f6299f4720
Fernando Tadashi Akimoto <tadashi@adobe.com>
2018-11-10 19:27:12 -0200

    Exercise (chapter): 26

--- js/opcoesDaPagina/btnAjuda.js
+++ js/opcoesDaPagina/btnAjuda.js
@@ -8,20 +8,19 @@ function randomColor() {
     ];
     const btnAjuda = document.querySelector('#btnAjuda');
     btnAjuda.addEventListener('click', function() {
-        msgs.forEach(function(msg) {
-            adicionarCartaoNoMural(msg);
-        });
-        // const xhr = new XMLHttpRequest();
-        // xhr.open('json', 'https://ceep.herokuapp.com/cartoes/instrucoes');
-        // /* xhr.responseType = 'json'; */
-        // xhr.send();
-        // xhr.addEventListener('load', function() {
-        //     const objeto = xhr.response;
-        //     const ajudas = objeto.instrucoes;
-        //     ajudas.forEach(function(ajuda) {
-        //         adicionarCartaoNoMural(ajuda);
-        //     });
+        // msgs.forEach(function(msg) {
+        //     adicionarCartaoNoMural(msg);
         // });
+        const xhr = new XMLHttpRequest();
+        xhr.open('json', 'https://ceep.herokuapp.com/cartoes/instrucoes');
+        xhr.send();
+        xhr.addEventListener('load', function() {
+            const objeto = xhr.response;
+            const ajudas = objeto.instrucoes;
+            ajudas.forEach(function(ajuda) {
+                adicionarCartaoNoMural(ajuda);
+            });
+        });
     });
     btnAjuda.classList.remove('no-js');
 })();
--- js/opcoesDaPagina/btnSync.js
+++ js/opcoesDaPagina/btnSync.js
@@ -4,14 +4,26 @@
         btnSync.addClass('botaoSync--esperando');
         btnSync.removeClass('botaoSync--sincronizado');
         const salvadorDeCartoes = new XMLHttpRequest();
-        salvadorDeCartoes.open('json', 'https://ceep.herokuapp.com/cartoes/salvar');
+        salvadorDeCartoes.open('POST', 'https://ceep.herokuapp.com/cartoes/salvar');
         salvadorDeCartoes.setRequestHeader('Content-Type', 'application/json');
         const cartoes = document.querySelectorAll('.cartao');
         const infosDoMural = {
             usuario: 'f.tadashi@gmail.com'
             , cartoes: Array.from($('.cartao')).map(function(cartao) {
-                
+
             })
         };
+        salvadorDeCartoes.send(JSON.stringify(infosDoMural));
+        salvadorDeCartoes.addEventListener('load', function() {
+            const response = JSON.parse(salvadorDeCartoes.response);
+            console.log(`${response.quantidade} cartões salvos em ${response.usuario}`);
+            btnSync.removeClass('botaoSync--esperando');
+            btnSync.addClass('botaoSync--sincronizado');
+        });
+        salvadorDeCartoes.addEventListener('error', function() {
+            btnSync.removeClass('botaoSync--esperando');
+            btnSync.addClass('botaoSync--deuRum');
+        });
     });
+    btnSync.removeClass('no-js');
 })();
